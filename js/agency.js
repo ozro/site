dfb2385d25e1faa137b3ebfc2f1a6f3bfe5cebfb
Oliver Zhang <ozz@andrew.cmu.edu>
2017-03-23 00:03:10 -0400

    Added .htaccess file with caching and vary encoding headers. Removed files and renamed files.

--- js/agency.js
+++ js/agency.js
@@ -1,10 +1,23 @@
 // Agency Theme JavaScript
 
-(function($) {
+/*jslint browser: true*/
+/*global $, jQuery*/
+(function ($) {
     "use strict"; // Start of use strict
+    $(".navbar-brand").hide();
 
+    $(window).bind("mousewheel mousedown DOMMouseScroll keyup", function () {$("html, body").stop(); });
+    
+    $(window).scroll(function () {    
+    var x = $(window).scrollTop();
+    if (x >= 100) {
+        $(".navbar-brand").show();
+    } else {
+        $(".navbar-brand").hide();
+    }})
+    
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -19,8 +32,8 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
+    $('.navbar-collapse ul li a').click(function () {
+        $('.navbar-toggle:visible').click();
     });
 
     // Offset for Main Navigation
@@ -28,6 +41,6 @@
         offset: {
             top: 100
         }
-    })
+    });
 
-})(jQuery); // End of use strict
+}(jQuery)); // End of use strict
